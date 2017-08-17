f7c13fe3f3ede55615c6424e29dc615304b8fd35
Joseph Joyce <joej326@gmail.com>
2017-08-16 21:22:56 -0700

    edited project rows

--- src/components/Paralax.js
+++ src/components/Paralax.js
@@ -30,15 +30,16 @@ export default class Paralax extends Component{
           <div className='projects-before-resume'>
             <h3 className='projects-mini'>Projects:</h3>
             <div className='project-container'>
-              <Project
-                link='https://www.youtube.com/watch?v=2aAHu8qtJdE'
-                title='Skateboard Clone'
-                thumbnail='http://i.imgur.com/4UV8LQM.jpg' />
-              <Project title='UC Davis Clone' thumbnail='http://i.imgur.com/wooXzXW.png' link='http://198.199.104.39:8011/' />
-              <Project title='Admin Dashboard' thumbnail='http://i.imgur.com/3mqY61Y.png' link='https://www.youtube.com/watch?v=j5z6mhGqv58' />
-              <Project title='Simple Weather App' thumbnail='http://i.imgur.com/v78Frs2.png' link='http://198.199.104.39:8033/' />
-              <Project title='Peet&#39;s Coffee Clone' thumbnail='http://i.imgur.com/ZPfOYXU.png' link='http://198.199.104.39:8017' />
-              <Project title='Amazon Clone (Coming Soon)' thumbnail='http://i.imgur.com/rOiG3Dd.png' link='#' />
+              <div className="project-row">
+                <Project link='https://www.youtube.com/watch?v=2aAHu8qtJdE' title='Skateboard Clone' thumbnail='http://i.imgur.com/4UV8LQM.jpg' />
+                <Project title='UC Davis Clone' thumbnail='http://i.imgur.com/wooXzXW.png' link='http://198.199.104.39:8011/' />
+                <Project title='Admin Dashboard' thumbnail='http://i.imgur.com/3mqY61Y.png' link='https://www.youtube.com/watch?v=j5z6mhGqv58' />
+              </div>
+              <div className="project-row">
+                <Project title='Simple Weather App' thumbnail='http://i.imgur.com/v78Frs2.png' link='http://198.199.104.39:8033/' />
+                <Project title='Peet&#39;s Coffee Clone' thumbnail='http://i.imgur.com/ZPfOYXU.png' link='http://198.199.104.39:8017' />
+                <Project title={['Amazon Clone', <br />, '(Coming Soon)']} thumbnail='http://i.imgur.com/rOiG3Dd.png' link='#' />
+              </div>
             </div>
             <h2 id='many-more'>Many more projects on my <a id='more-on-github' href='https://github.com/joej326'>Github</a>!</h2>
           </div>
